6228a9e2039797ea25a9f1012eda6a36e88c87a2
mhutter07 <martin.hutter@edu.fh-joanneum.at>
2019-01-09 11:57:48 +0100

    Repositories für die drei Model

---
+++ backend/src/main/java/at/fh/ima/swengs/cineFoxDB/model/HerstellerRepository.java
@@ -0,0 +1,14 @@
+package at.fh.ima.swengs.cineFoxDB.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+@RepositoryRestResource
+@Transactional(isolation = Isolation.READ_COMMITTED)
+public interface HerstellerRepository extends PagingAndSortingRepository<User, Long>, JpaRepository<User, Long>, CrudRepository<User, Long> {
+
+}
