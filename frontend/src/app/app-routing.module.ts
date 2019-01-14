4690c557a2359299d6d59c33c4128f42e824a69c
mhutter07 <martin.hutter@edu.fh-joanneum.at>
2019-01-14 16:33:45 +0100

    Die Fehler von Georg Schmitzer wurden ausgebessert

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {AuthGuard} from './auth.guard';
+import {PersonFormComponent} from './person-form/person-form.component';
 
 const routes: Routes = [
   {
     path: '', redirectTo: '/login', pathMatch: 'full'
+  },
+  {
+    path: 'person-form', component: PersonFormComponent, canActivate: [AuthGuard]
   },
   {
     path: 'login', component: LoginComponent
