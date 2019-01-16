0bfc894b28c35ad3b103b0bcf5dd9e2c3c4d95b5
Georg Schmitzer <georg.schmitzer@edu.fh-joanneum.at>
2019-01-16 10:01:49 +0100

    Update fpr HTML and CSS and Frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,9 @@ import {LoginComponent} from './login/login.component';
 import {AuthGuard} from './auth.guard';
 import {PersonFormComponent} from './person-form/person-form.component';
 import {MovieFormComponent} from './movie-form/movie-form.component';
+import {LandingpageComponent} from './landingpage/landingpage.component';
 
 const routes: Routes = [
-  {
-    path: '', redirectTo: '/login', pathMatch: 'full'
-  },
   {
     path: 'person-form', component: PersonFormComponent // , canActivate: [AuthGuard]
   },
@@ -20,6 +18,12 @@ const routes: Routes = [
   },
   {
     path: 'login', component: LoginComponent
+  },
+  {
+    path: '', component: LandingpageComponent
+  },
+  {
+    path: 'index', component: LandingpageComponent
   },
 ];
 
