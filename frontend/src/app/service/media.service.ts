b06d8e4667db2614efd0d9bedbe5ea9a3b79c301
Georg Schmitzer <georg.schmitzer@edu.fh-joanneum.at>
2019-01-21 15:06:43 +0100

    update backend serie; genre; media

---
+++ frontend/src/app/service/media.service.ts
@@ -0,0 +1,45 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Person} from '../api/person';
+import {map} from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getById(id: string) {
+    return this.http.get('/api/dto/person/' + id).pipe(map((res: any) => {
+      if (res.dayOfBirth) {
+        res.dayOfBirth = new Date(res.dayOfBirth);
+      }
+      return res;
+    }));
+  }
+
+  getAll() {
+    return this.http.get('/api/persons').pipe(
+      map((response: any) => {
+        return response._embedded.persons;
+      })
+    );
+  }
+
+  delete(person) {
+    return this.http.delete('/api/persons/' + person.id);
+  }
+
+  update(person: Person) {
+    return this.http.put('/api/dto/person/' + person.id, person);
+  }
+
+  create(person: Person) {
+    return this.http.post('/api/dto/person', person);
+  }
+
+}
+
