165723a07d9910474581a76c0135c2788126576d
GartnerF <gartner.florian@gmx.net>
2019-01-24 12:29:56 +0100

    removed standard error  message for better usability

--- frontend/src/app/httpinterceptor/error.interceptor.ts
+++ frontend/src/app/httpinterceptor/error.interceptor.ts
@@ -14,9 +14,9 @@ export class ErrorInterceptor implements HttpInterceptor {
     return next.handle(req).pipe(
       catchError((err: HttpErrorResponse) => {
         console.log('error.interceptor');
-        if (err instanceof HttpErrorResponse) {
+        /*if (err instanceof HttpErrorResponse) {
           this.toastrService.error(err.message);
-        }
+        }*/
         return throwError(err);
       })
     );
